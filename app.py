9f8d66ade125294fcbd043e602e8898650d59727
Raihan Nugroho Jauhari <raihan.nugrohojauhari@ai.astra.co.id>
2024-09-13 14:01:05 +0700

    update 2

--- app.py
+++ app.py
@@ -63,5 +63,8 @@ async def home():
     return {"message": "Hello, World! This is the main page."}
 
 # Run the app with Uvicorn's server
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
